a45c629ac96b9dcdd3af5f24d6281f636f2737f1
Miguel <miguelmvilela12@gmail.com>
2023-08-16 17:13:08 -0300

    Finalizado o CRUD de user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,9 @@ import UserControler from '../controllers/UserControler';
 
 const router = new Router();
 router.post('/', UserControler.store);
+router.get('/', UserControler.index);
+router.get('/:id', UserControler.show);
+router.put('/:id', UserControler.update);
+router.delete('/:id', UserControler.delete);
 
 export default router;
