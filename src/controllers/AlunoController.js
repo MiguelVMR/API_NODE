45d064406dfe456d637974db1c294e52218736ff
Miguel <miguelmvilela12@gmail.com>
2023-08-17 16:54:24 -0300

    Feito CRUD de alunos Com validação de login para as rotas de: Deletar, Atualizar e Criar

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,9 +1,102 @@
 import Aluno from '../models/Aluno';
 
 class AlunoController {
+  // Mostra todos os alunos
   async index(req, res) {
     const alunos = await Aluno.findAll();
     res.json(alunos);
+  }
+
+  // Criar alunos
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Mostrar um aluno
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Atualiza o aluno
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      const AlunoAtualizado = await aluno.update(req.body);
+
+      return res.json(AlunoAtualizado);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Deleta aluno
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+      await aluno.destroy();
+      return res.json({
+        apagado: true,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
   }
 }
 
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 
 import alunoControler from '../controllers/AlunoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 router.get('/', alunoControler.index);
+router.get('/:id', alunoControler.show);
+router.post('/', loginRequired, alunoControler.store);
+router.put('/:id', loginRequired, alunoControler.update);
+router.delete('/:id', loginRequired, alunoControler.delete);
 
 export default router;
