c2fa4a64d02cfaf46313d2a9bbce6711f9917ddf
Miguel <miguelmvilela12@gmail.com>
2023-08-18 17:32:04 -0300

    Implementado para salvar imagem na base de dados vinculado a um aluno

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import loginRequired from '../middlewares/loginRequired';
 
 import fotoControler from '../controllers/FotoController';
 
 const router = new Router();
-router.post('/', fotoControler.store);
+router.post('/', loginRequired, fotoControler.store);
 
 export default router;
